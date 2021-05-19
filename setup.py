204359dfc8e0581d0d2508fe7d88571f0952d717
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-19 16:36:37 +0100

    Depend on autoreduce_qp

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='autoreduction-frontend',
-    version='21.1',
+    version='21.1.1',
     description='The frontend of the ISIS Autoreduction service',
     author='ISIS Autoreduction Team',
     url='https://github.com/ISISScientificComputing/autoreduce-frontend/',
     install_requires=[
-        'autoreduce_utils==0.1.2', 'attrs==21.2.0', 'dash==1.20.0', 'dash_html_components==1.1.3',
-        'dash_core_components==1.16.0', 'Django==3.2.2', 'django_extensions==3.1.3', 'django_plotly_dash==1.6.3',
-        'django-user-agents==0.4.0', 'python-icat==0.18.1'
+        'autoreduce_db==0.1.2', 'autoreduce_utils==0.1.2', 'autoreduce_qp==21.1', 'attrs==21.2.0', 'dash==1.20.0',
+        'dash_html_components==1.1.3', 'dash_core_components==1.16.0', 'Django==3.2.2', 'django_extensions==3.1.3',
+        'django_plotly_dash==1.6.3', 'django-user-agents==0.4.0', 'python-icat==0.18.1'
     ],
     packages=find_packages(),
 )
