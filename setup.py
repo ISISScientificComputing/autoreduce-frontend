ed52757c7ff3a1e0e6dac979c4e4a6708b1c1fe3
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-07-27 10:19:25 +0100

    Add comments about matching the version

--- setup.py
+++ setup.py
@@ -19,15 +19,16 @@ for loc in data_locations:
     data_files.extend([f.split(f"{PACKAGE_NAME}/")[1] for f in glob(f"{loc}/**", recursive=True) if os.path.isfile(f)])
 print(data_files)
 
-setup(name=PACKAGE_NAME,
-      version="22.0.0.dev3",
-      description="The frontend of the ISIS Autoreduction service",
-      author="ISIS Autoreduction Team",
-      url="https://github.com/ISISScientificComputing/autoreduce-frontend/",
-      install_requires=[
-          "autoreduce_utils==22.0.0.dev3", "autoreduce_db==22.0.0.dev5", "autoreduce_qp==22.0.0.dev3", "Django==3.2.4",
-          "django_extensions==3.1.3", "django-user-agents==0.4.0", "djangorestframework==3.12.4"
-      ],
-      packages=find_packages(),
-      package_data={"": data_files},
-      entry_points={"console_scripts": ["autoreduce-webapp-manage = autoreduce_frontend.manage:main"]})
+setup(
+    name=PACKAGE_NAME,
+    version="22.0.0.dev3",  # when updating the version here make sure to also update webapp.D
+    description="The frontend of the ISIS Autoreduction service",
+    author="ISIS Autoreduction Team",
+    url="https://github.com/ISISScientificComputing/autoreduce-frontend/",
+    install_requires=[
+        "autoreduce_utils==22.0.0.dev3", "autoreduce_db==22.0.0.dev5", "autoreduce_qp==22.0.0.dev3", "Django==3.2.4",
+        "django_extensions==3.1.3", "django-user-agents==0.4.0", "djangorestframework==3.12.4"
+    ],
+    packages=find_packages(),
+    package_data={"": data_files},
+    entry_points={"console_scripts": ["autoreduce-webapp-manage = autoreduce_frontend.manage:main"]})
