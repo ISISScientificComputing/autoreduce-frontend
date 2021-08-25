7eea7836aad4aa9906d639b3ac46795e2f5f3589
Dagonite <up821535@myport.ac.uk>
2021-08-25 15:15:43 +0000

    Fix docstrings and spacing

--- autoreduce_frontend/reduction_viewer/view_utils.py
+++ autoreduce_frontend/reduction_viewer/view_utils.py
@@ -4,30 +4,26 @@
 # Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 # ############################################################################### #
-"""
-Utility functions for the view of django models
-"""
+"""Utility functions for the view of django models."""
 import functools
 import logging
 import os
 
 from autoreduce_db.reduction_viewer.models import Instrument
 from autoreduce_qp.queue_processor.reduction.service import ReductionScript
-
 from autoreduce_frontend.autoreduce_webapp.settings import DATA_ANALYSIS_BASE_URL
 
 LOGGER = logging.getLogger(__package__)
 
 
+# pylint:disable=no-member
 def deactivate_invalid_instruments(func):
-    """
-    Deactivate instruments if they are invalid
-    """
+    """Deactivate instruments if they are invalid."""
     @functools.wraps(func)
     def request_processor(request, *args, **kws):
         """
-        Function decorator that checks the reduction script for all active instruments
-        and deactivates any that cannot be found
+        Function decorator that checks the reduction script for all active
+        instruments and deactivates any that cannot be found.
         """
         instruments = Instrument.objects.all()
         for instrument in instruments:
@@ -42,24 +38,24 @@ def deactivate_invalid_instruments(func):
 
 
 def get_interactive_plot_data(plot_locations):
-    """
-    Get the data for the interactive plots from the saved JSON files
-    """
-    # get only JSON files
+    """Get the data for the interactive plots from the saved JSON files."""
     json_files = [location for location in plot_locations if location.endswith(".json")]
 
     output = {}
-
     for filepath in json_files:
         name = os.path.basename(filepath)
         with open(filepath, 'r') as file:
             data = file.read()
         output[name] = data
+
     return output
 
 
 def make_data_analysis_url(reduction_location: str):
-    """Makes a URL for the data.analysis website that will open the location of the data"""
+    """
+    Makes a URL for the data.analysis website that will open the location of the
+    data.
+    """
     if "/instrument/" in reduction_location:
         return DATA_ANALYSIS_BASE_URL + reduction_location.split("/instrument/")[1]
     else:
