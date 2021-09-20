d8ce20559671b8d348c9d207d791bf607aea70f5
Dagonite <up821535@myport.ac.uk>
2021-09-20 16:01:13 +0000

    Fix docstrings and formatting

--- autoreduce_frontend/autoreduce_webapp/wsgi.py
+++ autoreduce_frontend/autoreduce_webapp/wsgi.py
@@ -12,11 +12,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
-
+# pylint:disable=invalid-name
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autoreduce_frontend.autoreduce_webapp.settings")
-# pylint: disable=invalid-name
 application = get_wsgi_application()
