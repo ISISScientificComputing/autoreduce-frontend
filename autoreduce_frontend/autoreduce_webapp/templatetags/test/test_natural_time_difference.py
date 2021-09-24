75efe0717ec85e37482b71420853de0d9e46099d
Dagonite <up821535@myport.ac.uk>
2021-09-24 16:40:00 +0000

    Create test file for natural_time_difference

---
+++ autoreduce_frontend/autoreduce_webapp/templatetags/test/test_natural_time_difference.py
@@ -0,0 +1,10 @@
+# # ############################################################################### #
+# # Autoreduction Repository : https://github.com/ISISScientificComputing/autoreduce
+# #
+# # Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI
+# # SPDX - License - Identifier: GPL-3.0-or-later
+# # ############################################################################### #
+"""
+Unit tests for the script which determines the natural time difference between
+the start and end of a run.
+"""
