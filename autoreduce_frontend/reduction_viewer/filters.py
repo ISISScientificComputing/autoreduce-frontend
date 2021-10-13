02e10ff9cc6de9dd5c3a4fd6ee909482f0c0d72f
James Brookes <james.brookes@stfc.ac.uk>
2021-10-13 12:27:47 +0000

    add date range filter for search

--- autoreduce_frontend/reduction_viewer/filters.py
+++ autoreduce_frontend/reduction_viewer/filters.py
@@ -1,19 +1,22 @@
 from autoreduce_db.reduction_viewer.models import ReductionRun
+from django.forms.widgets import DateInput
 import django_filters
+from django_filters.filters import DateFromToRangeFilter
+from django_filters.widgets import RangeWidget
 
 
 class ReductionRunFilter(django_filters.FilterSet):
+    created = django_filters.DateFromToRangeFilter(widget=RangeWidget(attrs={
+        'type': 'date',
+        'placeholder': 'dd-mm-yyyy'
+    }))
+
     class Meta:
         model = ReductionRun
-        fields = [
-            'run_number',
-            'instrument',
-            'run_description',
-            'last_updated',
-        ]
+        fields = ['run_number', 'instrument', 'run_description']
 
     def __init__(self, *args, **kwargs):
         super(ReductionRunFilter, self).__init__(*args, **kwargs)
-        # at sturtup user doen't push Submit button, and QueryDict (in data) is empty
+        # doesn't push Submit button, QueryDict (in data) is empty so not all runs displayed at start
         if self.data == {}:
             self.queryset = self.queryset.none()
