fb614f10a96ee3cc6a6ef360a7fc1f5c60cfeca3
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-25 14:17:14 +0000

    Change test_rerun_jobs to use multiple variables

--- autoreduce_frontend/selenium_tests/tests/pages/test_rerun_jobs.py
+++ autoreduce_frontend/selenium_tests/tests/pages/test_rerun_jobs.py
@@ -12,7 +12,7 @@ class TestRerunJobsPage(NavbarTestMixin, BaseTestCase, FooterTestMixin, Accessib
     Test cases for the InstrumentSummary page when the Rerun form is NOT visible
     """
 
-    fixtures = BaseTestCase.fixtures + ["run_with_one_variable"]
+    fixtures = BaseTestCase.fixtures + ["run_with_multiple_variables"]
 
     @classmethod
     def setUpClass(cls):
@@ -22,8 +22,11 @@ class TestRerunJobsPage(NavbarTestMixin, BaseTestCase, FooterTestMixin, Accessib
         cls.data_archive = DataArchive([cls.instrument_name], 21, 21)
         cls.data_archive.create()
         cls.data_archive.add_reduction_script(cls.instrument_name, """print('some text')""")
-        cls.data_archive.add_reduce_vars_script(cls.instrument_name,
-                                                """standard_vars={"variable1":"test_variable_value_123"}""")
+
+        cls.data_archive.add_reduce_vars_script(
+            cls.instrument_name, """standard_vars={"variable_str":"value1","variable_int":123,"variable_float":123.321,
+            "variable_listint":[1,2,3],"variable_liststr":["a","b","c"],"variable_none":None,
+            "variable_empty":"","variable_bool":True}""")
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -44,9 +47,21 @@ class TestRerunJobsPage(NavbarTestMixin, BaseTestCase, FooterTestMixin, Accessib
 
     def test_reset_values_does_reset_the_values(self):
         """Test that the button to reset the variables to the values from the reduce_vars script works"""
-        self.page.variable1_field = "the new value in the field"
+        self.page.variable_str_field = "the new value in the field"
         self.page.reset_to_current_values.click()
 
         # need to re-query the driver because resetting replaces the elements
-        var_field = self.page.variable1_field
-        assert var_field.get_attribute("value") == "test_variable_value_123"
+        var_field = self.page.variable_str_field
+        assert var_field.get_attribute("value") == "value1"
+
+    def test_variables_appear_as_expected(self):
+        """
+        Test: Just opening the submit page and clicking rerun
+        """
+        assert self.page.variable_str_field_val == "value1"
+        assert self.page.variable_int_field_val == "123"
+        assert self.page.variable_float_field_val == "123.321"
+        assert self.page.variable_listint_field_val == "[1, 2, 3]"
+        assert self.page.variable_liststr_field_val == "['a', 'b', 'c']"
+        assert self.page.variable_none_field_val == "None"
+        assert self.page.variable_bool_field_val == "True"
