60d59eda332de1fb6191fc823473354732a1abab
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-26 21:56:49 +0100

    Fixed bad getting of reduction script

--- autoreduce_frontend/selenium_tests/utils.py
+++ autoreduce_frontend/selenium_tests/utils.py
@@ -68,7 +68,7 @@ def submit_and_wait_for_result(test, expected_runs=1):
             return True
         return False
 
-    WebDriverWait(test.driver, 30).until(runs_completed)
+    WebDriverWait(test.driver, 30).until(runs_completed, "Timed out while waiting for the runs to finish")
 
     # num_current_runs = ReductionRun.objects.filter(status=Status.get_completed()).count()
     # WebDriverWait(test.driver, 30).until(lambda _: ReductionRun.objects.filter(~Q(status=Status.get_completed())).count(
