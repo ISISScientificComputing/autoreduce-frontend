2cccea6c08467d766685b482cc976b34afe598f7
James Brookes <james.brookes@stfc.ac.uk>
2021-10-27 09:34:11 +0000

    keep selected search tab active on page reload

--- autoreduce_frontend/reduction_viewer/views.py
+++ autoreduce_frontend/reduction_viewer/views.py
@@ -617,15 +617,30 @@ def started_by_id_to_name(started_by_id=None):
 @check_permissions
 @render_with('search.html')
 def search(request):
-    run_list = ReductionRun.objects.all()
-    experiment_list = Experiment.objects.all()
+    run_list = ReductionRun.objects.none()
     run_description_qualifier = request.GET.get("run_description_qualifier", "contains")
     run_filter = ReductionRunFilter(request.GET, run_description_qualifier=run_description_qualifier, queryset=run_list)
+    run_table = ReductionRunSearchTable(run_list, order_by="-run_number")
+
+    experiment_list = Experiment.objects.none()
     experiment_filter = ExperimentFilter(request.GET, queryset=experiment_list)
-    run_table = ReductionRunSearchTable(run_filter.qs, order_by="-run_number")
-    RequestConfig(request, paginate={"per_page": 10}).configure(run_table)
-    experiment_table = ExperimentTable(experiment_filter.qs, order_by="-reference_number")
+    experiment_table = ExperimentTable(experiment_list, order_by="-reference_number")
     RequestConfig(request, paginate={"per_page": 10}).configure(experiment_table)
+
+    if 'run_number' in request.GET:
+        run_list = ReductionRun.objects.all()
+        run_description_qualifier = request.GET.get("run_description_qualifier", "contains")
+        run_filter = ReductionRunFilter(request.GET,
+                                        run_description_qualifier=run_description_qualifier,
+                                        queryset=run_list)
+        run_table = ReductionRunSearchTable(run_filter.qs, order_by="-run_number")
+        RequestConfig(request, paginate={"per_page": 10}).configure(run_table)
+    if "experiment_reference" in request.GET:
+        experiment_list = Experiment.objects.all()
+        experiment_filter = ExperimentFilter(request.GET, queryset=experiment_list)
+        experiment_table = ExperimentTable(experiment_filter.qs, order_by="-reference_number")
+        RequestConfig(request, paginate={"per_page": 10}).configure(experiment_table)
+
     options_form = SearchOptionsForm(initial={'pagination': request.GET.get('per_page', 10)})
     run_message = "Sorry, no runs found for this criteria."
     experiment_message = "Sorry, no experiments found for this criteria."
--- autoreduce_frontend/static/javascript/search_list.js
+++ autoreduce_frontend/static/javascript/search_list.js
@@ -3,29 +3,13 @@ function update_page(item) {
     document.location.href = document.location.href + "&per_page=" + per_page;
 }
 
-(function () {
-    'use strict';
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    console.log("tab shown...");
+    localStorage.setItem('activeTab', $(e.target).attr('href'));
+});
 
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        var id = $(this).parents('[role="tablist"]').attr('id');
-        var key = 'lastTag';
-        if (id) {
-            key += ':' + id;
-        }
-
-        localStorage.setItem(key, $(e.target).attr('href'));
-    });
-
-    $('[role="tablist"]').each(function (idx, elem) {
-        var id = $(elem).attr('id');
-        var key = 'lastTag';
-        if (id) {
-            key += ':' + id;
-        }
-
-        var lastTab = localStorage.getItem(key);
-        if (lastTab) {
-            $('[href="' + lastTab + '"]').tab('show');
-        }
-    });
-})();
+// read hash from page load and change tab
+var activeTab = localStorage.getItem('activeTab');
+if (activeTab) {
+    $('.nav-tabs a[href="' + activeTab + '"]').tab('show');
+}
