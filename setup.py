19642a195465fd0a3a7e458e1bb46181219546b5
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-11-10 08:44:40 +0000

    Changes version to latest one that needs to be published in pip
    The last one currently is dev5, so this changes the versions to dev6

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ print(data_files)
 
 setup(
     name=PACKAGE_NAME,
-    version="22.0.0.dev8",  # when updating the version here make sure to also update webapp.D
+    version="22.0.0.dev6",  # when updating the version here make sure to also update webapp.D
     description="The frontend of the ISIS Autoreduction service",
     author="ISIS Autoreduction Team",
     url="https://github.com/ISISScientificComputing/autoreduce-frontend/",
