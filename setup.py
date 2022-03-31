577c0e316953d1cb93b1b57804983f8921ff79aa
James Brookes <james.brookes@stfc.ac.uk>
2022-03-31 23:37:58 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ print(data_files)
 
 setup(
     name=PACKAGE_NAME,
-    version="22.0.0.dev19",  # when updating the version here make sure to also update webapp.D
+    version="22.0.0.dev20",  # when updating the version here make sure to also update webapp.D
     description="The frontend of the ISIS Autoreduction service",
     author="ISIS Autoreduction Team",
     url="https://github.com/autoreduction/autoreduce-frontend/",
